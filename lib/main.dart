7f390e56d9331ef3e93bc877d799d1523e9bbde4
gilprad <gilangpraditto@gmail.com>
2021-09-28 15:31:47 +0700

    adding state management for bottomnavbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:simple_movie_app/repository/favorited_repository.dart';
+import 'package:simple_movie_app/repository/tab_repository.dart';
 import 'package:simple_movie_app/screens/detail_page_screen.dart';
 
 import 'screens/home_screen.dart';
@@ -14,9 +16,19 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: DetailPageScreen(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => TabRepository(),
+        ),
+        // ChangeNotifierProvider(
+        //   create: (_) => FavoritedRepository(),
+        // )
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: HomeScreen(),
+      ),
     );
   }
 }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,18 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:provider/provider.dart';
 import 'package:simple_movie_app/components/drawer.dart';
+import 'package:simple_movie_app/repository/tab_repository.dart';
+import 'package:simple_movie_app/screens/detail_page_screen.dart';
 import 'package:simple_movie_app/screens/explore_screen.dart';
 import 'package:simple_movie_app/screens/favorited_screen.dart';
 import 'package:simple_movie_app/screens/watchlist_screen.dart';
 
-class HomeScreen extends StatefulWidget with ChangeNotifier{
-   HomeScreen({Key? key}) : super(key: key);
+class HomeScreen extends StatefulWidget {
+  HomeScreen({Key? key}) : super(key: key);
 
   @override
   _HomeScreenState createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
+class _HomeScreenState extends State<HomeScreen> with ChangeNotifier {
   static List<Widget> pages = [
     ExploreScreen(),
     FavoritedScreen(),
@@ -20,45 +23,49 @@ class _HomeScreenState extends State<HomeScreen> {
   ];
 
   String appBarTitle = 'Simple Movie App';
-  
-  
+
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        bottomNavigationBar: BottomNavigationBar(
-          items: const <BottomNavigationBarItem>[
-            BottomNavigationBarItem(
-              icon: FaIcon(FontAwesomeIcons.compass),
-              label: 'Explore',
-            ),
-            BottomNavigationBarItem(
-              icon: FaIcon(FontAwesomeIcons.heart),
-              label: 'Favorited',
-            ),
-            BottomNavigationBarItem(
-              icon: FaIcon(FontAwesomeIcons.list),
-              label: 'Watch List',
-            )
-          ],
+    return Consumer<TabRepository>(builder: (context, tabRepository, child) {
+      return SafeArea(
+        child: Scaffold(
+          bottomNavigationBar: BottomNavigationBar(
+            onTap: (index) {
+              tabRepository.goToTab(index);
+            },
+            items: const <BottomNavigationBarItem>[
+              BottomNavigationBarItem(
+                icon: FaIcon(FontAwesomeIcons.compass),
+                label: 'Explore',
+              ),
+              BottomNavigationBarItem(
+                icon: FaIcon(FontAwesomeIcons.heart),
+                label: 'Favorited',
+              ),
+              BottomNavigationBarItem(
+                icon: FaIcon(FontAwesomeIcons.list),
+                label: 'Watch List',
+              )
+            ],
+          ),
+          drawer: CustomDrawer(),
+          appBar: AppBar(
+            title: Text(appBarTitle),
+            actions: [
+              IconButton(
+                  onPressed: () {
+                    showSearch(context: context, delegate: SearchScreen());
+                  },
+                  icon: const FaIcon(FontAwesomeIcons.search))
+            ],
+          ),
+          body: IndexedStack(
+            index: tabRepository.selectedTab,
+            children: pages,
+          ),
         ),
-        drawer: CustomDrawer(),
-        appBar: AppBar(
-          title: Text(appBarTitle),
-          actions: [
-            IconButton(
-                onPressed: () {
-                  showSearch(context: context, delegate: SearchScreen());
-                },
-                icon: const FaIcon(FontAwesomeIcons.search))
-          ],
-        ),
-        body: IndexedStack(
-          index: 0,
-          children: pages,
-        ),
-      ),
-    );
+      );
+    });
   }
 }
 
@@ -68,25 +75,62 @@ class SearchScreen extends SearchDelegate {
 
   @override
   List<Widget>? buildActions(BuildContext context) {
-    // TODO: implement buildActions
-    throw UnimplementedError();
+    return [
+      IconButton(
+        onPressed: () {
+          query = '';
+        },
+        icon: FaIcon(
+          FontAwesomeIcons.times,
+        ),
+      )
+    ];
   }
 
   @override
   Widget? buildLeading(BuildContext context) {
-    // TODO: implement buildLeading
-    throw UnimplementedError();
+    return IconButton(
+        onPressed: () {
+          close(context, null);
+        },
+        icon: AnimatedIcon(
+          icon: AnimatedIcons.menu_arrow,
+          progress: transitionAnimation,
+        ));
   }
 
   @override
   Widget buildResults(BuildContext context) {
-    // TODO: implement buildResults
-    throw UnimplementedError();
+    return ListView.separated(
+        itemBuilder: (context, index) => ListTile(
+              onTap: () {
+                Navigator.of(context).push(
+                    MaterialPageRoute(builder: (_) => DetailPageScreen()));
+              },
+              leading: Icon(Icons.ac_unit),
+              title: Text('Title'),
+            ),
+        separatorBuilder: (context, index) => Divider(),
+        itemCount: 2);
   }
 
   @override
   Widget buildSuggestions(BuildContext context) {
-    // TODO: implement buildSuggestions
-    throw UnimplementedError();
+    return ListView.builder(
+      itemCount: 2,
+      itemBuilder: (context, index) => ListTile(
+        onTap: () {
+          showResults(context);
+        },
+        title: Text(
+          'Title',
+        ),
+        leading: FaIcon(FontAwesomeIcons.history),
+        trailing: IconButton(
+          icon: FaIcon(FontAwesomeIcons.trash),
+          onPressed: () {},
+        ),
+      ),
+    );
   }
 }
